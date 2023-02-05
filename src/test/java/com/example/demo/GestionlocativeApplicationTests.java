7045e69c2875eb5803ef0ddcb0f1db7995ed6f39
AHADJITSE Yawo Florent Mathis <yawo.ahadjitse@ipnetinstitute.com>
2023-02-05 22:33:23 +0000

    1- Initialisation du projet gestion locative

---
+++ src/test/java/com/example/demo/GestionlocativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionlocativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
